c27658c62663c1db04716860246b09aec3c54a15
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2025-10-18 14:22:22 +0200

    Created base GameLogger

---
+++ Mandatory2DGameFramework/Patterns/GameLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.Patterns
+{
+    public class GameLogger
+    {
+        private static GameLogger instance = new GameLogger();
+        public static GameLogger Instance
+        {
+            get { return instance; }
+        }
+        private GameLogger()
+        {
+        }
+
+
+        
+
+    }
+}
