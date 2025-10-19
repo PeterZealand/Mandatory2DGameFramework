bfcafc077ec490b73792e80630c23077a5fe217a
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2025-10-19 15:56:03 +0200

    GameLogger Written, First try.

--- Mandatory2DGameFramework/Patterns/GameLogger.cs
+++ Mandatory2DGameFramework/Patterns/GameLogger.cs
@@ -14,12 +14,62 @@ namespace Mandatory2DGameFramework.Patterns
         {
             get { return instance; }
         }
+        private TraceSource traceSource;
+        private TraceListener? debugListener = null;
         private GameLogger()
         {
+            traceSource = new TraceSource("GameLogger", SourceLevels.All);
+            traceSource.Switch = new SourceSwitch("DebugListener", SourceLevels.All.ToString());
         }
 
+        public void AddListener(TraceListener listener)
+        {
+            traceSource.Listeners.Add(listener);
+        }
+        public void RemoveListener(TraceListener listener)
+        {
+            traceSource.Listeners.Remove(listener);
+        }
+        public void SetDefaultLogLevel(SourceLevels level)
+        {
+            traceSource.Switch.Level = level;
+        }
+        public void AddDebugLogging()
+        {
+            if (debugListener == null)
+            {
+                debugListener = new ConsoleTraceListener();
+                traceSource.Listeners.Add(debugListener);
+            }
+        }
+        public void RemoveDebugLogging()
+        {
+            if (debugListener != null)
+            {
+                traceSource.Listeners.Remove(debugListener);
+                debugListener = null;
+            }
+        }
+        public void Stop()
+        {
+            traceSource.Close();
+        }
+        public void LogInfo(string message)
+        {
+            traceSource.TraceEvent(TraceEventType.Information, 2, message);
+        }
+        public void LogWarning(string message)
+        {
+            traceSource.TraceEvent(TraceEventType.Warning, 2, message);
+        }
+        public void LogError(string message)
+        {
+            traceSource.TraceEvent(TraceEventType.Error, 2, message);
+        }
 
-        
-
+        public void LogCritical(string message)
+        {
+            traceSource.TraceEvent(TraceEventType.Critical, 2, message);
+        }
     }
 }
